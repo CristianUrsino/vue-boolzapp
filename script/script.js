c1cfcf6fc6f05e58704762154b69d2f741e0355f
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-11-03 11:06:13 +0100

    MILESTONE 2 COMPLITE

--- script/script.js
+++ script/script.js
@@ -7,14 +7,27 @@ const { createApp } = Vue;
     data() {
       return {
         contacts : contactsList,
+        activeContact : 0,
       }
     },
     methods:{
-      foundLastMessage(i){
+      LastMessageReceived(i){
+        let lastM = this.lastMessage(i, 'received');
+        return lastM.message;
+      },
+      isTheActive(i){
+        this.activeContact = i;
+      },
+      dataLastMassage(i){
+        let lastM = this.lastMessage(i, 'received');
+        return lastM.date;
+      },
+      lastMessage(i, status){
+        if(status === '') return this.contacts[i].messages[this.contacts[i].messages.length - 1];
         let j = this.contacts[i].messages.length - 1;
         while( j >= 0){
-          if (this.contacts[i].messages[j].status === 'sent' ){
-            return this.contacts[i].messages[j].message;
+          if (this.contacts[i].messages[j].status === status ){
+            return this.contacts[i].messages[j];
           }
           j--;
         }
